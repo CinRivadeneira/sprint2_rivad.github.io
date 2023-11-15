312bfb6d5f93bae32f5e0abe57f1d99d5c7e38f8
CinRivadeneira <cistri092@gmail.com>
2023-11-14 21:01:32 -0300

    js de las tazas

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-let azulBtn = document.getElementById("azul");
-let grisBtn = document.getElementById("gris");
-let imgChange = document.getElementById("imgchange");
+let alfombraAzulBtn = document.getElementById("azula");
+let alfombraGrisBtn = document.getElementById("grisa");
+let imgChange = document.getElementById("imgchangea");
 
-azulBtn.onclick = function(){
-    imgchange.src = "../media/repuestos/alfombraazul.jpeg";
+alfombraAzulBtn.onclick = function(){
+    imgchangea.src = "../media/repuestos/alfombraazul.jpeg";
 }
 
-grisBtn.onclick = function(){
-    imgchange.src = "../media/repuestos/alfombragris.jpeg";
+alfombraGrisBtn.onclick = function(){
+    imgchangea.src = "../media/repuestos/alfombragris.jpeg";
+}
+
+
+
+
+let tazaAzulBtn = document.getElementById("azult");
+let tazaRojoBtn = document.getElementById("rojot");
+let imgChanget = document.getElementById("imgchanget");
+
+tazaAzulBtn.onclick = function(){
+    imgchanget.src = "../media/repuestos/tazaazul.jpeg";
+}
+console.log("hola");
+
+tazaRojoBtn.onclick = function(){
+    imgchanget.src = "../media/repuestos/tazaroja.jpeg";
 }
